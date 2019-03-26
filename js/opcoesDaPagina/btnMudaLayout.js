e0851995f08dacd1c0906e4c433fc2160d0ee503
Your NameLeonardo <l.gc@hotmail.com>
2019-03-25 22:32:47 -0300

    primeira aula javascript

---
+++ js/opcoesDaPagina/btnMudaLayout.js
@@ -0,0 +1,22 @@
+const btn=document.querySelector('#btnMudaLayout')
+
+function MudaTexto(){
+
+    if(btn.textContent=='Blocos'){
+        btn.textContent='Linhas'
+    }else{
+        btn.textContent='Blocos'
+    }
+
+}
+
+
+
+btn.addEventListener('click', MudaTexto)
+const mural=document.querySelector('.mural')
+
+function MudaLayout() {
+    mural.classList.toggle('mural--linha')
+}
+
+btn.addEventListener('click', MudaLayout)
