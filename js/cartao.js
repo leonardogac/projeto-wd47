18dc082981581d7c722cf1fdae38ed84cb6e21d9
Your NameLeonardo <l.gc@hotmail.com>
2019-03-27 22:50:24 -0300

    aula 3 javascript

---
+++ js/cartao.js
@@ -0,0 +1,41 @@
+;(function(){
+    const cartoes = document.querySelectorAll('.cartao')
+
+    cartoes.forEach(function(cartao){
+        cartao.addEventListener('focusin', function(){
+            cartao.classList.add('cartao--focado')
+        })
+
+        cartao.addEventListener('focusout', function(){
+            cartao.classList.remove('cartao--focado')
+        })
+        
+        cartao.addEventListener('change', function mudacor(event){
+            const elementoSelecionado = event.target
+            const isRadioTipo=elementoSelecionado.classList.contains('opcoesDoCartao-radioTipo')
+            console.log(elementoSelecionado, isRadioTipo, cartao)
+            if(isRadioTipo){
+                cartao.style.backgroundColor = elementoSelecionado.value
+            }
+        })
+
+        cartao.addEventListener('keydown', function clicarComEnter(event){
+            if(event.target.classList.contains('opcoesDoCartao-opcao')
+            && (event.key === 'Enter' || event.key === ' ')){
+                event.target.click()
+
+            }
+        })
+
+        cartao.addEventListener('click', function(event){
+           const elementoSelecionado = event.target
+            if(elementoSelecionado.classList.contains('opcoesDoCartao-remove')){
+                cartao.classList.add('cartao--some')
+                cartao.addEventListener('transitionend', function(){
+                    cartao.remove()
+                })
+            }
+        })
+
+    })
+})()
